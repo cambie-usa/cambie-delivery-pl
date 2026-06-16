a0c7c1bb55c30ec59525d16b9c712fb61101ce0c
cambie-usa <dfoley@cambie.com>
2026-06-16 15:52:27 -0500

    Update config.js

--- cambie-delivery-pl/public/config.js
+++ cambie-delivery-pl/public/config.js
@@ -7,5 +7,5 @@
 // in the database controls what can be read or written.
 // =================================================================
 
-const SUPABASE_URL  = 'YOUR_SUPABASE_URL';   // e.g. https://xxxx.supabase.co
-const SUPABASE_ANON = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL  = 'https://ublhirsllacegddiiorp.supabase.co';   // e.g. https://xxxx.supabase.co
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVibGhpcnNsbGFjZWdkZGlpb3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NDIwNTEsImV4cCI6MjA5NzIxODA1MX0.e5yK86-dppZKTE9_QrgIdLkC_3a64SJOHRkWOVxQG14';
